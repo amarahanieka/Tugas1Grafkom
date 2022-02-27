32a1d1aeb4e29089a63ba04193cefee423a3d902
amarahanieka <13519082@std.stei.itb.ac.id>
2022-02-27 14:20:53 +0700

    line shows up but only the first one

--- src/new/shapefunctions/LineFunctions.js
+++ src/new/shapefunctions/LineFunctions.js
@@ -37,6 +37,7 @@ class LineFunctions {
         this.nextClickEvent = (e) => {
             if (this.isDrawing){
                 this.endpoint = this.setPoint(e);
+                // console.log("masuk ke next click event")
                 this._fireEvent = ("endPointCreated", this.endpoint);
                 
             }
@@ -79,12 +80,15 @@ class LineFunctions {
     };
 
     setPoint(e) {
-        var x = e.clientX;
-        var y = e.clientY;
-        x = (x - this.canvas.width / 2) / (this.canvas.width / 2);
-        y = (this.canvas.height / 2 - y) / (this.canvas.height / 2);
+        // var x = e.clientX;
+        // var y = e.clientY;
+        // x = (x - this.canvas.width / 2) / (this.canvas.width / 2);
+        // y = (this.canvas.height / 2 - y) / (this.canvas.height / 2);
 
-        var pts = [x, y];
+        const x = (2 * e.clientX) / this.canvas.width - 1;
+        const y = (-2 * e.clientY) / this.canvas.height + 1;
+
+        var pts = [x, y, 0];
         return pts;
     };
 
--- src/new/shapes/Line.js
+++ src/new/shapes/Line.js
@@ -7,12 +7,20 @@ class Line {
 
     render(context, vb, cb){
         context.bindBuffer(context.ARRAY_BUFFER, vb);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([this.point1, this.point2]), gl.STATIC_DRAW);
-        
+        var p1 = this.point1.toString()+',0.0,'
+        var p2 = this.point2.toString()+',0.0,'
+        var semua = [];
+        var aa = p1+p2
+        var b = aa.split(',').map(Number);
+        semua.push(b)
+        console.log('semua'+semua)
+        context.bufferData(gl.ARRAY_BUFFER, new Float32Array(semua), gl.STATIC_DRAW);
         context.bindBuffer(context.ARRAY_BUFFER, cb);
+        context.bindBuffer(context.ARRAY_BUFFER, null)
 
         // gl.clear( gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.LINES, 0, 2);
         gl.drawArrays(gl.POINTS, 0, 2);
+        console.log("harusnya kalo sampe sini dah nongol woi")
     }
 }
