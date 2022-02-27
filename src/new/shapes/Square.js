b56b0ce00556f120ab803bb94a4ad32378447cf2
Melita <13519063@std.stei.itb.ac.id>
2022-02-27 21:52:22 +0700

    save load, gambar ui, help, fix square drawing

--- src/new/shapes/Square.js
+++ src/new/shapes/Square.js
@@ -2,8 +2,19 @@ class Square {
     constructor(point1, point3){
         const canvasXYratio = 500/800
         this.point1 = point1;
-        let dist = point1[1]-point3[1]
-        this.point3 = [point1[0]+dist*canvasXYratio,point1[1]-dist,0] 
+        let dist = Math.abs(point1[1]-point3[1])
+        let distx = Math.abs(point1[0]-point3[0])
+
+        let mirrorX = point1[0] > point3[0] ? -1 : 1;
+        let mirrorY = point1[1] > point3[1] ? -1 : 1;
+        
+        if(dist>distx){
+            this.point3 = [point1[0]+mirrorX*dist*canvasXYratio,point1[1]+mirrorY*dist,0] 
+        }
+        else{
+            this.point3 = [point1[0]+mirrorX*distx,point1[1]+mirrorY*distx*(1/canvasXYratio),0] 
+        }
+
         this.point2 = [point1[0],this.point3[1],0]
         this.point4 = [this.point3[0],point1[1],0]
         // this.color = color;
@@ -11,10 +22,10 @@ class Square {
 
     render(context, vb, cb){
         context.bindBuffer(context.ARRAY_BUFFER, vb);
-        var p1 = this.point1.toString()+',0.0,'
-        var p2 = this.point2.toString()+',0.0,'
-        var p3 = this.point3.toString()+',0.0,'
-        var p4 = this.point4.toString()+',0.0,'
+        var p1 = this.point1.toString()
+        var p2 = this.point2.toString()
+        var p3 = this.point3.toString()
+        var p4 = this.point4.toString()
         var semua = [];
         var aa = p1+p2+p3+p4
         var b = aa.split(',').map(Number);
--- src/new/tes.js
+++ src/new/tes.js
@@ -135,7 +135,7 @@ function main() {
     }
 
     document.getElementById("ControlButton").addEventListener("click", function() {
-        document.getElementById("whichShape").innerHTML = "using control tool!";
+        controlHelp();
         resetBorderColor();
         this.classList.add("selectedtool");
         isDrawing = false;
@@ -144,7 +144,7 @@ function main() {
     });
 
     document.getElementById("LineButton").addEventListener("click", function() {
-        document.getElementById("whichShape").innerHTML = "a line!";
+        lineHelp();
         resetBorderColor();
         this.classList.add("selectedtool");
         isDrawing = true;
@@ -154,7 +154,7 @@ function main() {
     });
     
     document.getElementById("SquareButton").addEventListener("click", function() {
-        document.getElementById("whichShape").innerHTML = "a square!";
+        squareHelp();
         resetBorderColor();
         this.classList.add("selectedtool");
         isDrawing = true;
@@ -164,7 +164,7 @@ function main() {
     });
 
     document.getElementById("RectangleButton").addEventListener("click", function() {
-        document.getElementById("whichShape").innerHTML = "a rectangle!";
+        rectHelp();
         resetBorderColor();
         this.classList.add("selectedtool");
         isDrawing = true;
@@ -173,19 +173,153 @@ function main() {
         console.log("tesssssss");
     });
 
+    document.getElementById("PolygonButton").addEventListener("click", function() {
+        polygonHelp();
+        resetBorderColor();
+        this.classList.add("selectedtool");
+        isDrawing = true;
+        deactivateAll();
+        //polygonFunctions.activate();
+    });
+
     document.getElementById("UndoButton").addEventListener("click", function() {
         undoList.push(listObject.pop());
         render();
     });
 
     document.getElementById("ClearButton").addEventListener("click", function() {
-        document.getElementById("whichShape").innerHTML = "nothing.";
+        clearAll();
+    });
+
+    function clearAll(){
+        resetBorderColor();
+        showHelp();
         gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT|gl.STENCIL_BUFFER_BIT);
-        lineFunctions.deactivate();
-        console.log("tesssssss hapos");
+        deactivateAll();
         isDrawing = true;
         listObject.length = 0;
         semua.length = 0;
+    }
+
+    // File Menu buttons
+    document.getElementById("SaveButton").addEventListener("click", function() {
+        var savedata = document.createElement("a");
+        savedata.setAttribute("download", "canvas_data.cad");
+        var datatext = "data:text/cad,";
+        listObject.forEach(element => {
+            datatext += element.constructor.name;
+            datatext += "\n";
+            if(element.constructor.name == "Square" || element.constructor.name == "Rectangle")
+            {
+                datatext += element.point1.toString();
+                datatext += ";"
+                datatext += element.point2.toString();
+                datatext += ";"
+                datatext += element.point3.toString();
+                datatext += ";"
+                datatext += element.point4.toString();
+            }
+            else
+            {
+                datatext += element.point1.toString();
+                datatext += ";"
+                datatext += element.point2.toString();
+            }
+            datatext += "\n";
+        });
+        savedata.setAttribute("href", datatext);
+        document.body.appendChild(savedata);
+        savedata.click();
+    });
+
+    document.getElementById("LoadButton").addEventListener("click", function() {
+        var input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.cad';
+
+        input.onchange = e => { 
+            var f = e.target.files[0]; 
+            var read = new FileReader();
+            read.readAsText(f,'UTF-8');
+            read.onload = readerEvent => {
+                var isifile = readerEvent.target.result;
+                arrfile = isifile.split('\n')
+                console.log(arrfile);
+                
+                clearAll();
+
+                for (let k = 0; k < arrfile.length; k+=2) {
+                    let shape = arrfile[k];
+                    let arrtemp = arrfile[k+1].split(';');
+                    let arrpoints = []
+                    console.log("parse ",k)
+
+                    for (let j = 0; j < arrtemp.length; j+=1) {
+                        let tempval = arrtemp[j].split(',')
+                        let x = parseFloat(tempval[0], 32)
+                        let y = parseFloat(tempval[1], 32)
+                        arrpoints.push([x,y,0]);
+                    }
+
+                    if(shape == "Line"){
+                        let obj = new Line(arrpoints[0], arrpoints[1]);
+                        listObject.push(obj);
+                    }
+
+                    else if(shape == "Square"){
+                        let obj = new Square(arrpoints[0], arrpoints[2]);
+                        listObject.push(obj);
+                    }
+
+                    else if(shape == "Rectangle"){
+                        let obj = new Rectangle(arrpoints[0], arrpoints[2]);
+                        listObject.push(obj);
+                    }
+                    else{
+                        //..Polygon...
+                    }
+                }
+
+                render();
+            }
+        }
+
+        input.click();
+    });
+
+    // Misc. Menu buttons
+    function showHelp(){
+        document.getElementById("whichShape").innerHTML = "<img src='img/icon_help.png'>Instructions / Help";
+        document.getElementById("toolDetail").innerHTML = "<p>How to use: <ol> <li>Click one of the tools from the menu on the right side of the screen. Detailed instructions on how to use the tool will show up on the bottom of the screen.</li> <li>Use the tool by clicking inside the canvas area.</li> <li>The [Show help] button can be used to show this instructions on the screen again.</li> </ol> </p> <p>List of tools: <ul> <li><b>Control:</b> Use to pick a shape and edit its colors/position.</li> <li><b>Line:</b> Creates a straight line.</li> <li><b>Square:</b> Creates a square.</li> <li><b>Rectangle:</b> Creates a rectangle.</li> <li><b>Polygon:</b> Creates a polygon.</li> <li><b>Undo Draw:</b> Deletes last created shape.</li> <li><b>Clear:</b> Cleares the screen (WARNING: action cannot be undone).</li> <li><b>Load file:</b> Loads canvas from a saved file.</li> <li><b>Save current file:</b> Saves current canvas into a file.</li> <li><b>Fill color:</b> Changes the fill color of a shape (square, rectangle, polygon) according to color input.</li> <li><b>Stroke color:</b> Changes the stroke color of a shape (applicable to all shapes) according to color input.</li> <li><b>Show help:</b> Shows this help menu.</li> </ul> </p>";
+    }
+
+    function controlHelp(){
+        document.getElementById("whichShape").innerHTML = "<img src='img/icon_control.png'>Control Tool";
+        document.getElementById("toolDetail").innerHTML = "<p>Click on a shape on the canvas to start editing the shape. You can change the position of the shape, move the vertices, or change the shape color. To change the color, simply pick the shape with the [Control Tool], then pick a color using the color input. Finally, select either [Fill color] or [Stroke color] depending on which one you want to change.</p>";
+    }
+
+    function lineHelp(){
+        document.getElementById("whichShape").innerHTML = "<img src='img/icon_line.png'>Line Tool";
+        document.getElementById("toolDetail").innerHTML = "<p>Creates a straight line. Click on the canvas to mark a startpoint, then click again on the canvas to mark an endpoint.</p>";
+    }
+
+    function squareHelp(){
+        document.getElementById("whichShape").innerHTML = "<img src='img/icon_square.png'>Square Tool";
+        document.getElementById("toolDetail").innerHTML = "<p>Creates a square. Click on the canvas to mark a vertex, then click again on the canvas to mark a second vertex. A square will be made based on these two vertices.</p>";
+    }
+
+    function rectHelp(){
+        document.getElementById("whichShape").innerHTML = "<img src='img/icon_rect.png'>Rectangle Tool";
+        document.getElementById("toolDetail").innerHTML = "<p>Creates a rectangle. Click on the canvas to mark a vertex, then click again on the canvas to mark a second vertex. A rectangle will be made based on these two vertives.</p>";
+    }
+
+    function polygonHelp(){
+        document.getElementById("whichShape").innerHTML = "<img src='img/icon_polygon.png'>Polygon Tool";
+        document.getElementById("toolDetail").innerHTML = "<p>Creates a polygon. Click on the canvas to mark the startpoint of the polygon, then keep clicking on the canvas for every vertex of the polygon. Right-click on the canvas to close the polygon.";
+    }
+
+    document.getElementById("HelpButton").addEventListener("click", function() {
+        showHelp();
     });
 
 
